9fadb2c34503036d3544e856dd15c4017f484911
Solomon X. <solomonxiewise@gmail.com>
2026-04-05 13:26:09 +0800

    Implement hello_03_3lights: cycle blue/red/green on GPIO18/19/21

--- hello-esp32/hello_03_3lights.ino
+++ hello-esp32/hello_03_3lights.ino
@@ -1,13 +1,27 @@
-#define LED_PIN 2  // GPIO2
-#define PAUSE 250  // delay in ms
+// Program 3: Cycle 3 LEDs (blue/red/green) one at a time
+// Wiring: GPIO18 = Blue, GPIO19 = Red, GPIO21 = Green, all sharing one GND rail.
+
+#define LED_BLUE  18
+#define LED_RED   19
+#define LED_GREEN 21
+#define PAUSE 500  // delay in ms per color
 
 void setup() {
-  pinMode(LED_PIN, OUTPUT);
+  pinMode(LED_BLUE, OUTPUT);
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(LED_PIN, HIGH);   // Turn on LED
+  digitalWrite(LED_BLUE, HIGH);
   delay(PAUSE);
-  digitalWrite(LED_PIN, LOW);   // Turn LED off
+  digitalWrite(LED_BLUE, LOW);
+
+  digitalWrite(LED_RED, HIGH);
   delay(PAUSE);
+  digitalWrite(LED_RED, LOW);
+
+  digitalWrite(LED_GREEN, HIGH);
+  delay(PAUSE);
+  digitalWrite(LED_GREEN, LOW);
 }
